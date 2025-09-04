de3efbf9ea2200a6daf83035e6f76c089768096a
A Normal MF <0kaya02huseyin02@gmail.com>
2025-09-05 01:51:11 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      textAlign: "center",
+      marginTop: "100px",
+      fontFamily: "Arial, sans-serif"
+    }}>
+      <h1>🚀 Discord Bot Panel</h1>
+      <p>Merhaba 👋 Bu panel Vercel üzerinde başarıyla çalışıyor.</p>
+      <p>Buradan botuna ait özellikler ekleyebilirsin.</p>
+    </div>
+  );
+}
