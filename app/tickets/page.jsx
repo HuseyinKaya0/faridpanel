dcd20814ed3ccb3048dd392f1e27ce9c88f6cb4d
A Normal MF <0kaya02huseyin02@gmail.com>
2025-09-05 03:10:36 +0300

    Create page.jsx

---
+++ app/tickets/page.jsx
@@ -0,0 +1,8 @@
+export default function TicketsPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Ticket Sayfası</h1>
+      <p className="opacity-70">Yakında buraya ticket sistemi gelecek 🚀</p>
+    </div>
+  );
+}
