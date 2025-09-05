4dbf4de96dff638498a04b856013981c8b51d677
A Normal MF <0kaya02huseyin02@gmail.com>
2025-09-05 03:11:23 +0300

    Create page.jsx

---
+++ app/bans/page.jsx
@@ -0,0 +1,8 @@
+export default function BansPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Banlılar</h1>
+      <p className="opacity-70">Burada banlı kullanıcılar listelenecek.</p>
+    </div>
+  );
+}
