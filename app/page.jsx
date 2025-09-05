73703bbc77b3891804c72c4429235b693949026e
A Normal MF <0kaya02huseyin02@gmail.com>
2025-09-05 03:09:47 +0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h1 className="text-4xl font-bold">İşlem Alanı</h1>
+    </div>
+  );
+}
